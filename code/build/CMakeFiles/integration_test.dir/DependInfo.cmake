9c03193dcce582f992bf26539b89d41dd11a24c5
Erisae <xiayuhan18@mails.ucas.ac.cn>
2023-03-05 03:03:44 -0500

    added integration test

---
+++ code/build/CMakeFiles/integration_test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/arika/Desktop/ShellRecorder/code/methods/input_parser.cpp" "CMakeFiles/integration_test.dir/methods/input_parser.cpp.o" "gcc" "CMakeFiles/integration_test.dir/methods/input_parser.cpp.o.d"
+  "/Users/arika/Desktop/ShellRecorder/code/methods/launch_record.cpp" "CMakeFiles/integration_test.dir/methods/launch_record.cpp.o" "gcc" "CMakeFiles/integration_test.dir/methods/launch_record.cpp.o.d"
+  "/Users/arika/Desktop/ShellRecorder/code/tests/integration_test.cpp" "CMakeFiles/integration_test.dir/tests/integration_test.cpp.o" "gcc" "CMakeFiles/integration_test.dir/tests/integration_test.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/arika/Desktop/ShellRecorder/code/build/_deps/googletest-build/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/Users/arika/Desktop/ShellRecorder/code/build/_deps/googletest-build/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- code/build/_deps/googletest-build/googlemock/CMakeFiles/gmock.dir/flags.make
+++ code/build/_deps/googletest-build/googlemock/CMakeFiles/gmock.dir/flags.make
@@ -6,7 +6,7 @@ CXX_DEFINES =
 
 CXX_INCLUDES = -I/Users/arika/Desktop/ShellRecorder/code/build/_deps/googletest-src/googlemock/include -I/Users/arika/Desktop/ShellRecorder/code/build/_deps/googletest-src/googlemock -isystem /Users/arika/Desktop/ShellRecorder/code/build/_deps/googletest-src/googletest/include -isystem /Users/arika/Desktop/ShellRecorder/code/build/_deps/googletest-src/googletest
 
-CXX_FLAGSarm64 =  -arch arm64 -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX13.1.sdk  -DGTEST_HAS_PTHREAD=1   -std=c++14
+CXX_FLAGSarm64 = --coverage -arch arm64 -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX13.1.sdk  -DGTEST_HAS_PTHREAD=1   -std=c++14
 
-CXX_FLAGS =  -arch arm64 -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX13.1.sdk  -DGTEST_HAS_PTHREAD=1   -std=c++14
+CXX_FLAGS = --coverage -arch arm64 -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX13.1.sdk  -DGTEST_HAS_PTHREAD=1   -std=c++14
 
