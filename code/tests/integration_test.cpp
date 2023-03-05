0d06b49ddc83fdd4ea7253edac9ac5b9ebb95b38
Erisae <xiayuhan18@mails.ucas.ac.cn>
2023-03-05 04:16:38 -0500

    fix autobuild error

--- code/tests/integration_test.cpp
+++ code/tests/integration_test.cpp
@@ -1,35 +1,28 @@
+#include "../headers/input_parser.h"
 #include "../headers/launch_record.h"
 
 #include <fstream>
 #include <gtest/gtest.h>
 
-const std::string output_file = "./record_output.sh";
-const std::string input_file  = "./record_input.sh";
-TEST(LaunchRecordTest, Input){
+static int argc1 = 4;
+static const char* argv1[] = {"shellrecorder", "-r", "-f", "inte_test"};
+
+TEST(IntegrationTest, all)
+{
+    const InputParser parser1(argc1, argv1);
+    const std::string output_file = parser1.getCmdOption("-f");
     // redirect input
+    const std::string input_file  = "./record_input.sh";
     std::ifstream commands(input_file);
     std::streambuf* cin_buf = std::cin.rdbuf();
     std::cin.rdbuf(commands.rdbuf());
+    if(parser1.cmdOptionExists("-r"))
+        launchRecord(output_file.data());
 
-    // successfully record
-    EXPECT_TRUE(launchRecord("./record_output"));
-}
-
-TEST(LaunchRecordTest, Content){
-    // right content
-    std::ifstream commands(input_file);
     std::string commands_content((std::istreambuf_iterator<char>(commands)),
                              std::istreambuf_iterator<char>());
     std::ifstream records(output_file);
     std::string records_content((std::istreambuf_iterator<char>(records)),
                                 std::istreambuf_iterator<char>());
     EXPECT_STREQ(commands_content.c_str(), records_content.c_str());
-}
-
-
-TEST(LaunchRecordTest, Permission){
-    // right permission
-    std::string cmd = std::string("/bin/bash -c '[ -x ") + output_file + std::string(" ]'");
-    int result = std::system(cmd.data());
-    EXPECT_EQ(result, 0);
 }
